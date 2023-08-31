c32e306184b107a69145a549326341290d158074
wooblydog <defalt9474@gmail.com>
2023-09-01 00:02:54 +0500

    Исправление SQL

--- sql.sql
+++ sql.sql
@@ -1,16 +1,17 @@
 CREATE DATABASE blog;
 
 CREATE TABLE posts (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     title VARCHAR(255),
     body TEXT
 );
 
 CREATE TABLE comments (
-    id INT PRIMARY KEY ,
-    post_id INT REFERENCES posts(id) ON DELETE CASCADE,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    post_id INT NOT NULL,
     name VARCHAR(255),
     email VARCHAR(255),
-    body TEXT
+    body TEXT,
+    FOREIGN KEY (post_id) REFERENCES posts (id)
 );
